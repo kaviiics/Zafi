574baef605b21a4778d56e71013b46cf70c33bcc
kaviiics <blankahorvath900@gmail.com>
2026-09-23 16:30:39 +0200

    alap enumok letrehozasa

--- Zafi/main.cpp
+++ Zafi/main.cpp
@@ -1,6 +1,11 @@
 ﻿#include <print>
+#include <iostream>
+#include "types.h"
 
 int main()
 {
-	std::print("Mark szeretnel velem szexelni?");
+	//std::print("Mark szeretnel velem szexelni?");
+
+	Piece x = Queen;
+	std::cout << x;
 }
---
+++ Zafi/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum Piece {
+	Pawn,
+	Knight,
+	Bishop,
+	Rook,
+	Queen,
+	King
+};
+
+enum Color {
+	White,
+	Black
+};
